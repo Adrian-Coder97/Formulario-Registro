f4fa9eeea06fb4835a1a5c44b6bc4486289fddba
Adrian-Coder97 <adrian_lch@outlook.com>
2021-07-08 02:36:09 -0500

    integracion de php mailer pt2

--- procesar_registro.php
+++ procesar_registro.php
@@ -1,9 +1,16 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
 /*----------------------------------MOSTRAR LOS ELEMENTOS (CONSULTA)--------------------------------------*/
+if (isset($_POST['submit'])) {
 
-
-if ($_POST) {
-    include_once "conexion.php";
     /*recibir los datos*/
 
     $nombre_us = $_POST["nombre_us"];
@@ -51,28 +58,65 @@ if ($_POST) {
     }
 
     if ($name_error == '' && $email_error == '' && $pass_error == '' && $repass_error == '') {
-        /*enviarlos a la base de datos*/
+        /*si todo esta bien enviarlos a la base de datos*/
+        include_once "conexion.php";
 
-        $buenpass = password_hash($contra_us, PASSWORD_DEFAULT, ['cost' => 10]);
+        $vkey = md5(time() . $nombre_us); //generar vkey con el timepo actual y el nombre del usuario 
+        $buenpass = password_hash($contra_us, PASSWORD_DEFAULT, ['cost' => 10]); //encriptar pass
 
-        $sql_agregar = "INSERT INTO accounts (username, email, mi_password) VALUES (?,?,?)"; //signos de interrogacion por seguridad
+        $sql_agregar = "INSERT INTO accounts (username, mi_password, email, vkey) VALUES (?,?,?,?)"; //signos de interrogacion por seguridad
         $sentencia_agregar = $pdo->prepare($sql_agregar);
-        $sentencia_agregar->execute(array($nombre_us, $email_us, $buenpass)); //en el array van el el mismo orden que irian en los signos de interrogracion 
-        session_start();
+        $sentencia_agregar->execute(array($nombre_us, $buenpass, $email_us, $vkey)); //en el array van el el mismo orden que irian en los signos de interrogracion 
 
-        $_SESSION['success'] = "exito";
-        header("location:thankyou.php"); //recargar la pagina cuando se envien los datos 
-        echo "datos agregados" . "<br>";
-        echo "DATA:" . "<br>";
-        echo $nombre_us . "<br>";
-        echo $email_us . "<br>";
-        echo $contra_us . "<br>";
-        echo $repContra_us . "<br>";
-        echo "ERRORS:" . "<br>";
-        echo $name_error . "<br>";
-        echo $email_error . "<br>";
-        echo $pass_error . "<br>";
-        echo $repass_error . "<br>";
+        if ($sentencia_agregar) { //si se agrega a la bd hacer esto 
+            session_start();
+            $_SESSION['success'] = "exito";
+
+            //ENVIAR EL EMAIL 
+            $mail = new PHPMailer(true);
+
+            try {
+                //Server settings
+                $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+                $mail->isSMTP();                                            //Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'PONER TU EMAIL AQUI';                     //SMTP username
+                $mail->Password   = 'PONER TU CONTRASEÑA AQUI';                               //SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                //Recipients
+                $mail->setFrom('PONER TU EMAIL AQUI', 'PONER TU TITULO AQUI');
+                $mail->addAddress($email_us);     //Add a recipient
+
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Verificacion de tu cuenta de Task tracker';
+                $mail->Body    = '<a href="http://localhost/NOSE/verify.php?vkey=' . $vkey . '">Activar Cuenta </a>';
+
+                $mail->send();
+                echo 'Message has been sent';
+                echo $vkey;
+                header("Location: thankyou.php");
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                header("Location: index.php");
+            }
+            /* echo "datos agregados" . "<br>";
+            echo "DATA:" . "<br>";
+            echo $nombre_us . "<br>";
+            echo $email_us . "<br>";
+            echo $contra_us . "<br>";
+            echo $repContra_us . "<br>";
+            echo "ERRORS:" . "<br>";
+            echo $name_error . "<br>";
+            echo $email_error . "<br>";
+            echo $pass_error . "<br>";
+            echo $repass_error . "<br>";*/
+        } else {
+            echo "ERROR";
+        }
     } else {
         header("location:index.php");
         echo "datos no agregados" . "<br>";
